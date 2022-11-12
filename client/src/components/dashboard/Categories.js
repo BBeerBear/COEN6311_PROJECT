c83f850dc6c668919c940a1a11b393c1258d750c
unknown <seyfkkk@yahoo.fr>
2022-11-11 23:42:09 -0500

    Add files related to Dashboard

---
+++ client/src/components/dashboard/Categories.js
@@ -0,0 +1,34 @@
+import { categories } from './CategoriesList';
+import * as actions from '../../actions';
+import { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Categories extends Component {
+  render() {
+    return (
+      <nav>
+        <div className='nav-wrapper'>
+          <div className='col s12'>
+            {categories.map((category) => (
+              <a
+                href='#'
+                onClick={() =>
+                  // this.props.fetctNewsFromDB({ categories: [category.value] })
+                  this.props.fetctNewsFromGoogleTrends({
+                    categories: [category.value],
+                    geo: 'US',
+                  })
+                }
+                className='breadcrumb'
+              >
+                {category.label}
+              </a>
+            ))}
+          </div>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default connect(null, actions)(Categories);
