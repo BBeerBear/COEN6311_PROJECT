a337d02e34abdb92406fdb2f813e653b261b968b
Limin <xiongx659@gmail.com>
2022-11-13 20:34:25 -0500

    modify the layout

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -6,8 +6,12 @@ import NewsList from '../news/NewsList';
 const Dashboard = () => {
   return (
     <div class='row'>
-      <Categories />
-      <NewsList />
+      <div class='col s2'></div>
+
+      <div class='col s10'>
+        <Categories />
+        <NewsList />
+      </div>
     </div>
   );
 };
