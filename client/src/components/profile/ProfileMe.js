4ddf5a5fd6300d2587e00bf60ece844ab9c6ff90
Daphne-Susan <d_saju@live.concordia.ca>
2022-11-15 13:56:28 -0500

    profile card

--- client/src/components/profile/ProfileMe.js
+++ client/src/components/profile/ProfileMe.js
@@ -1,87 +1,70 @@
 import React, { Component } from 'react';
+import "./ProfileStyle.css";
 import { connect } from 'react-redux';
+// import avatar from "../images/image-rita.png";
 import * as actions from '../../actions';
 
 class ProfileMe extends Component {
-
   renderContent() {
-    // if (this.props.profile && this.props.auth && this.props.profile.profile) {
-    // console.log(this.props.profile.profile);
-    <div id='profile-card' class='card'>
-      <div class='card-image waves-effect waves-block waves-light '>
-        <img
-          class='activator '
-          src='https://cdn.windowsreport.com/wp-content/uploads/2022/05/Coconut-and-Azuki-in-Casual-Wear.jpg'
-          alt='user background'
-        />
-      </div>
-      <div class='card-content'>
-        <img
-          src={this.props.auth.picture}
-          alt=''
-          class='circle responsive-img activator card-profile-image'
-        />
-        {/* <a class='btn-floating activator btn-move-up waves-effect waves-light darken-2 right'>
-          <i class='mdi-action-account-circle'></i>
-        </a> */}
-
-        <span class='card-title activator grey-text text-darken-4'>
-          {this.props.auth.name}
-        </span>
-        {/* <p>
-          <i class='mdi-action-perm-identity cyan-text text-darken-2'></i>{' '}
-          Project Manager
-        </p>
-        <p>
-          <i class='mdi-action-perm-phone-msg cyan-text text-darken-2'></i> +1
-          (612) 222 8989
-        </p> */}
-        <p>
-          <i class='mdi-communication-email cyan-text text-darken-2'></i>{' '}
-          {this.props.auth.email}
-        </p>
-        {/* <p>
-            <i class='mdi-communication-email cyan-text text-darken-2'></i>{' '}
-            {this.props.profile.profile.country}
-          </p> */}
-      </div>
-      {/* <div class='card-reveal'>
-        <span class='card-title grey-text text-darken-4'>
-          Roger Waters <i class='mdi-navigation-close right'></i>
-        </span>
-        <p>Here is some more information about this card.</p>
-        <p>
-          <i class='mdi-action-perm-identity cyan-text text-darken-2'></i>{' '}
-          Project Manager
-        </p>
-        <p>
-          <i class='mdi-action-perm-phone-msg cyan-text text-darken-2'></i> +1
-          (612) 222 8989
-        </p>
-        <p>
-          <i class='mdi-communication-email cyan-text text-darken-2'></i>{' '}
-          {this.props.auth.email}
-        </p>
-        <p>
-          <i class='mdi-social-cake cyan-text text-darken-2'></i> 18th June
-          1990
-        </p>
-        <p>
-          <i class='mdi-device-airplanemode-on cyan-text text-darken-2'></i>{' '}
-          BAR - AUS
-        </p>
-      </div> */}
-    </div>;
-    // } else {
-    //   return <></>;
-    // }
+    // console.log(this.props.auth.name);
+    switch (this.props.auth) {
+      case null:
+        return;
+      case false:
+        return;
+      default: {
+        return [
+          <div className="card-container">
+            <header>
+              <a href='#!user'>
+                <img class='circle' src={this.props.auth.picture} />
+              </a>
+            </header>
+            <h1 className="UserCard">
+            </h1>
+            {/* <h2 className="normal-text">{props.city}</h2> */}
+            <div className="User">
+              <div className="UserName">
+                <h1 className="smaller-text">Name:</h1>
+                <a href='#!name'>
+                  <span class='name'>{this.props.auth.name}</span>
+                </a>
+              </div>
+              <div className="Email">
+                <h1 className="smaller-text">Email:
+                <a href='#!email'>
+                  <span class='email'>{this.props.auth.email}</span>
+                </a>
+                </h1>
+              </div>
+              {/* <div className="Categories">
+                <h1 className="smaller-text">Liked Categories:
+                <a href='#!email'>
+                  <span class='email'>{this.props.profile.profile[0].preferredCategories}</span>
+                </a>
+                </h1>
+              </div> */}
+            </div>
+          </div>
+        ];
+      }
+    }
   }
-
   render() {
-    return this.renderContent();
+    return (
+      <>
+        <ul id='profilecard-1' class='profileme profileme-fixed'>
+          {this.renderContent()}
+        </ul>
+      </>
+    );
   }
 }
-function mapStateToProps({ auth, profile }) {
-  return { auth, profile };
+//destruct state
+function mapStateToProps({ activity, auth, profile }) {
+  return { activity, auth, profile};
 }
+
 export default connect(mapStateToProps, actions)(ProfileMe);
+
+
