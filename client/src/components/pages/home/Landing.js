fe4881fedb18dd015a9b3cfe78b8e07b6e0bd395
lifanfan001 <784459493@qq.com>
2022-11-23 14:09:51 -0500

    landing page beautify

--- client/src/components/pages/home/Landing.js
+++ client/src/components/pages/home/Landing.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import './Landing.css';
+// import { Button } from './Button'
+
 
 const Landing = () => {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <h1>Welcome to Trending News Social Application</h1>
+    // <div style={{ textAlign: 'center' }}>
+    //   <h1>Welcome to Trending News Social Application hahah</h1>
+    // </div>
+
+    <div className='landing-container'>
+    {/* <img src='news.jpg' alt='news'></img> */}
+    <img src="../../news.jpg" alt="news" width="500" height="600"/>
+    <h1>Trending News, your best news application!</h1>
+    <p>Join us now!</p>
+    <div className='signup-btns'>
+
+      <button className='btns' buttonStyle='btn--outline'
+      buttonSize='btn--large'>
+        Login with Google
+      </button>
+
+      <button className='btns' buttonStyle='btn--primary'
+      buttonSize='btn--large'>
+        Login with Facebook
+      </button>
+     </div>
     </div>
-  );
+      );
 };
 
 export default Landing;
