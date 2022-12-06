bf28f9618481a348c3c4ac0be1babc64f1553ba1
gitcode-sam <mastersam1412@gmail.com>
2022-12-06 16:44:21 -0500

    graphs

---
+++ client/public/sketch.js
@@ -0,0 +1,53 @@
+
+// csv file we want to load
+let filename = 'data/AnnualProjected-Modeled.csv';
+
+// all of your code should be inside this command
+d3.csv(filename).then(function(loadedData) {
+  
+  // see if our data loaded correctly
+  // how data in file is formatted)
+  console.log(loadedData);
+  
+  // empty lists for our data and the labels
+  let data =   [];
+  let labels = [];
+  
+  // for-loop to load the data from the
+  // file into our lists
+  for (let i=0; i<loadedData.length; i++) {
+    console.log(loadedData[i]);
+    
+    // get the year and mean temp for each listing
+    // note: the 'keys' here correspond to the headers
+    // in our file and need to be typed exactly
+    let year =     loadedData[i].year;
+    let meanTemp = loadedData[i].rcp45_weighted_mean;
+    console.log(meanTemp);
+    
+    // add the year to our labels
+    labels.push(year);
+    
+    // and mean temp to the data
+    data.push(meanTemp);    
+  }
+  
+  // basic line chart settings
+  let options = {
+    type: 'line',
+    data: {
+      labels: labels,  // the labels we loaded!
+      datasets: [{
+        data: data,    // the data we loaded!
+        fill: false,
+        pointRadius: 0,
+        pointHoverRadius: 0,
+        borderColor: 'rgb(100,100,100)'
+      }]
+    }
+  };
+  
+  // create our chart!
+  let chart = new Chart(document.getElementById('canvas'), options);
+});
+
